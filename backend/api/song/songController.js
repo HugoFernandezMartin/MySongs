7bdc27a45f9f2c8be706a96c9c0d88c1b87e357e
HugoFernandezMartin <fernanmhugo@gmail.com>
2025-10-08 18:29:46 +0200

    Authentication middlewares implemented

--- backend/api/song/songController.js
+++ backend/api/song/songController.js
@@ -1,7 +1,7 @@
 const makeResponse = require("../../commons/models/response.js");
-const { createSong, getSongs } = require("./songRepository.js");
+const { getSongs, searchSongs } = require("./songRepository.js");
 
-async function getSongsController(req, res) {
+async function GetSongsController(req, res) {
   try {
     const { author, genre, album } = req.query;
     const songs = await getSongs(author, genre, album);
@@ -15,4 +15,24 @@ async function getSongsController(req, res) {
   }
 }
 
-module.exports = getSongsController;
+async function SearchSongController(req, res) {
+  try {
+    const { q } = req.query;
+
+    let songs;
+    if (!q || q.trim() === "") {
+      songs = await getSongs();
+    } else {
+      songs = await searchSongs(q);
+    }
+
+    res
+      .status(200)
+      .json(makeResponse(true, "Songs retrieved succesfully", songs, 200));
+  } catch (error) {
+    res
+      .status(500)
+      .json(makeResponse(false, "Error retrieving songs", error.message, 500));
+  }
+}
+module.exports = { GetSongsController, SearchSongController };
